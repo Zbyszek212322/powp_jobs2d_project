a0879397b01ff5c4182d11ac9180f4b0540937db
Zbyszek212322 <zbyszrad@poczta.wp.pl>
2019-10-27 17:31:20 +0100

    command pkt 1, 2

---
+++ src/main/java/edu/kis/powp/jobs2d/command/OperateToCommand.java
@@ -0,0 +1,25 @@
+package edu.kis.powp.jobs2d.command;
+
+import edu.kis.powp.jobs2d.Job2dDriver;
+
+public class OperateToCommand implements DriverCommand{
+
+    private int x;
+    private int y;
+    private Job2dDriver job2dDriver;
+
+    public OperateToCommand(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public void execute() {
+        job2dDriver.operateTo(x, y);
+    }
+
+    public void setJob2dDriver(Job2dDriver job2dDriver) {
+        this.job2dDriver = job2dDriver;
+    }
+}
